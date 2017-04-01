5444d19f9790717382776f7f2a62b68c5da6ca8b
Sergio Hurtarte <hurtartesergio@gmail.com>
2017-03-31 22:59:01 -0600

    Initial commit

---
+++ app/src/main/java/com/sergio/chejinais/platzigram/LoginActivity.java
@@ -0,0 +1,36 @@
+package com.sergio.chejinais.platzigram;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.sergio.chejinais.platzigram.view.ContainerActivity;
+import com.sergio.chejinais.platzigram.view.CreateAccountActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+
+    public void goCreateAccount(View view){
+
+
+        Intent intent = new Intent(this, CreateAccountActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void goContainer(View view){
+
+
+        Intent intent = new Intent(this, ContainerActivity.class);
+        startActivity(intent);
+
+    }
+
+}
---
+++ app/src/main/java/com/sergio/chejinais/platzigram/adapter/PictureAdapterRecyclerView.java
@@ -0,0 +1,82 @@
+package com.sergio.chejinais.platzigram.adapter;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.sergio.chejinais.platzigram.R;
+import com.sergio.chejinais.platzigram.model.Picture;
+import com.sergio.chejinais.platzigram.view.PictureDetailActivity;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+/**
+ * Created by sergio on 29/03/17.
+ */
+
+public class PictureAdapterRecyclerView  extends RecyclerView.Adapter<PictureAdapterRecyclerView.PictureViewHolder>{
+
+    private ArrayList<Picture> pictures;
+    private int resource;
+    private Activity activity;
+
+    public PictureAdapterRecyclerView(ArrayList<Picture> pictures, int resource, Activity activity) {
+        this.pictures = pictures;
+        this.resource = resource;
+        this.activity = activity;
+    }
+
+    @Override
+    public PictureViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(resource,parent,false);
+
+
+        return new PictureViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(PictureViewHolder holder, int position) {
+        Picture picture = pictures.get(position);
+        holder.usernameCard.setText(picture.getUserName());
+        holder.timeCard.setText(picture.getTime());
+        holder.likeNumberCard.setText(picture.getLike_numer());
+        Picasso.with(activity).load(picture.getPicture()).into(holder.pictureCard);
+
+        holder.pictureCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(activity, PictureDetailActivity.class);
+                activity.startActivity(intent);
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return pictures.size();
+    }
+
+    public class PictureViewHolder extends RecyclerView.ViewHolder{
+
+        private ImageView pictureCard;
+        private TextView usernameCard;
+        private TextView timeCard;
+        private TextView likeNumberCard;
+
+        public PictureViewHolder(View itemView) {
+            super(itemView);
+
+            pictureCard     = (ImageView) itemView.findViewById(R.id.picture_card);
+            usernameCard    =(TextView) itemView.findViewById(R.id.userNameCard);
+            timeCard        = (TextView) itemView.findViewById(R.id.timeCard);
+            likeNumberCard  = (TextView) itemView.findViewById(R.id.likeNumberCard);
+        }
+    }
+}
